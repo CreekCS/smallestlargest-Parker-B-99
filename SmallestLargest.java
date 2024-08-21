cb70c08671e3f719ce86f76a9e9c9a45a148eac2
Parker Brunkan <parkerbrunkan@icloud.com>
2024-08-21 19:44:48 +0000

    Completed SmallestLargest Assignment

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,30 @@
-//put your name here
+//Parker Brunkan
 public class SmallestLargest{
     public static void main(String[] args){
+        int [] numbers = new int[10];
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = (int)((Math.random() * 100) +1);
+        }
+
+        int small = smallestValue(numbers);
+        int big = largestValue(numbers);
+        System.out.print("Largest : " + big + "\nSmallest: " + small + "\n");
+    }
+    public static int smallestValue(int [] nums){
+        int j = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < j)
+                j = nums[i];
+        }
+        return j;
 
     }
-    public static int smallestValue(){
-
-    }
-    public static int largestValue(){
-        
+    public static int largestValue(int [] nums){
+        int j = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > j)
+                j = nums[i];
+        }
+        return j;
     }
 }
